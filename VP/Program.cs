6d4ca4f30274c660ce89872504dbae2774ecaafa
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-01-12 11:05:46 +0100

    .

--- VP/Program.cs
+++ VP/Program.cs
@@ -2,6 +2,16 @@
 using Raylib_cs;
 
 Raylib.InitWindow(1024, 768, "Game");
+Raylib.SetTargetFPS(60);
 
+Texture2D playerTexture = Raylib.LoadTexture("Circle.png");
+Rectangle player = new Rectangle(100, 50, playerTexture.width, playerTexture.height);
 
-Console.ReadLine();
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.WHITE);
+    Raylib.DrawTexture(playerTexture, 50, 50, Color.BLACK);
+
+    Raylib.EndDrawing();
+}
