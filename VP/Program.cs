b5cee05e532e5983ac3d2dcb36abfc4db7ae888f
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-02-08 11:09:38 +0100

    .

--- VP/Program.cs
+++ VP/Program.cs
@@ -14,9 +14,12 @@ Rectangle snake = new Rectangle(768, 300, snakeTexture.width, snakeTexture.heigh
 
 bool menu = true;
 bool fightSelect = false;
+
 bool goblinFight = false;
+int goblinHP = 100;
+
+
 bool snakeFight = false;
-bool bossFight = false;
 
 float speed = 3f;
 Vector2 movement = new Vector2();
@@ -25,76 +28,77 @@ Rectangle Start = new Rectangle(100, 20, 100, 20);
 
 while (!Raylib.WindowShouldClose())
 {
-    if(menu == true)
+    if (menu == true)
     {
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.WHITE);
-    Raylib.DrawRectangle(256, 192, 500, 100, Color.BLACK);
-    if(Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
-    {
-        menu = false;
-        fightSelect = true;
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
+        Raylib.DrawRectangle(256, 192, 500, 100, Color.BLACK);
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+        {
+            menu = false;
+            fightSelect = true;
+        }
+
+        Raylib.EndDrawing();
     }
 
-    Raylib.EndDrawing();
-    }
-   
-    if(fightSelect == true)
+    if (fightSelect == true)
     {
-    Raylib.BeginDrawing();
-    
-    
-    Raylib.ClearBackground(Color.WHITE);
-    Raylib.DrawTexture(playerTexture, (int)player.x, (int)player.y, Color.WHITE);
-    
-    movement = ReadMovement(speed);
-    player.x += movement.X;
-    player.y += movement.Y;
+        Raylib.BeginDrawing();
 
 
-    Raylib.DrawTexture(goblinTexture, 512, 384, Color.WHITE);
-    Raylib.DrawTexture(snakeTexture, 768, 300, Color.WHITE);
+        Raylib.ClearBackground(Color.WHITE);
+        Raylib.DrawTexture(playerTexture, (int)player.x, (int)player.y, Color.WHITE);
 
-    Raylib.EndDrawing();
+        movement = ReadMovement(speed);
+        player.x += movement.X;
+        player.y += movement.Y;
+
+
+        Raylib.DrawTexture(goblinTexture, 512, 384, Color.WHITE);
+        Raylib.DrawTexture(snakeTexture, 768, 300, Color.WHITE);
+
+        Raylib.EndDrawing();
     }
 
-    if(fightSelect == true)
+    if (fightSelect == true)
     {
-      if(Raylib.CheckCollisionRecs(player, goblin))
-      {
-        fightSelect = false;
-        goblinFight = true;
-      }
-      if(Raylib.CheckCollisionRecs(player, snake))
-      {
-        fightSelect = false;
-        snakeFight = true;
-      }
+        if (Raylib.CheckCollisionRecs(player, goblin))
+        {
+            fightSelect = false;
+            goblinFight = true;
+        }
+        if (Raylib.CheckCollisionRecs(player, snake))
+        {
+            fightSelect = false;
+            snakeFight = true;
+        }
     }
 
-    if(goblinFight == true)
+    if (goblinFight == true)
     {
-      Raylib.BeginDrawing();
+        Raylib.BeginDrawing();
 
-      Raylib.ClearBackground(Color.DARKBROWN);
-      Raylib.DrawTexture(goblinTexture, 768, 192, Color.WHITE);
+        Raylib.ClearBackground(Color.DARKBROWN);
+        Raylib.DrawTexture(goblinTexture, 768, 192, Color.WHITE);
+        Raylib.DrawRectangle(64, 512, 896, 256, Color.GRAY);
 
-      Raylib.EndDrawing();
+        Raylib.EndDrawing();
     }
 
 
 
-static Vector2 ReadMovement(float speed)
-{
-  Vector2 movement = new Vector2();
-  
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) movement.Y = -speed;
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) movement.Y = speed;
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) movement.X = -speed;
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) movement.X = speed;
+    static Vector2 ReadMovement(float speed)
+    {
+        Vector2 movement = new Vector2();
 
-  return movement;
-}
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) movement.Y = -speed;
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) movement.Y = speed;
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) movement.X = -speed;
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) movement.X = speed;
+
+        return movement;
+    }
 
 
 }
